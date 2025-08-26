179c00d96bf26f9df3d7e15ededb940deee01189
Enrique-290 <enri290@gmail.com>
2025-08-26 15:07:48 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 
-const CACHE_NAME = 'dinamita-pos-v2';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); });
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request))); });
+const CACHE='dinamita-pos-v3';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
